0d5d33fbb2e0f7828aaabd06f36707ae39b26e52
salvaem21 <salva.em21@gmail.com>
2024-04-22 09:46:48 +0200

    tfg

--- tfg/src/main/java/com/salvaceloisma/tfg/repository/SolicitudRepository.java
+++ tfg/src/main/java/com/salvaceloisma/tfg/repository/SolicitudRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.salvaceloisma.tfg.domain.Solicitud;
 
 @Repository
-public interface SolicitudRepository extends JpaRepository<Solicitud,Long> {
+public interface SolicitudRepository extends JpaRepository<Solicitud,String> {
     List<Solicitud> findByIdSolicitud(String idSolicitud);
     List<Solicitud> findByNumeroConvenio(Integer numeroConvenio);
 }
