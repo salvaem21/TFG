faf003e797bdbf248971ced1cd68f2f3c4d4ac85
salvaem21 <salva.em21@gmail.com>
2024-04-22 09:51:58 +0200

    tfg

---
+++ tfg/src/main/java/com/salvaceloisma/tfg/enumerados/EstadoSolicitud.java
@@ -0,0 +1,9 @@
+package com.salvaceloisma.tfg.enumerados;
+
+public enum EstadoSolicitud {
+    PENDIENTE_JEFATURA,
+    PENDIENTE_ADJUNTAR_ARCHIVO,
+    PENDIENTE_FIRMA_DIRECCION,
+    PENDIENTE_FIRMA_JEFATURA,
+    FINALIZADO_DIRECCION
+}
