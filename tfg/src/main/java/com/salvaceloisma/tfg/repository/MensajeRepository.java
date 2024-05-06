7a45058b28e279cce1e5ab37a6db6c0087b12d85
salvaem21 <salva.em21@gmail.com>
2024-05-06 10:57:13 +0200

    tfg 06-05
    IMPLEMENTACION BASICA MENSAJERIA PROFESOR - JEFATURA
    
    (FALTA HACER LA LOGICA ENTRE TODOS)

---
+++ tfg/src/main/java/com/salvaceloisma/tfg/repository/MensajeRepository.java
@@ -0,0 +1,14 @@
+package com.salvaceloisma.tfg.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.salvaceloisma.tfg.domain.Mensaje;
+import com.salvaceloisma.tfg.domain.Usuario;
+
+@Repository
+public interface MensajeRepository extends JpaRepository<Mensaje, Long> {
+    List<Mensaje> findByDestinatario(Usuario destinatario);
+}
