1b98d34049f20779ba2f9337c19d600c7b0217e5
salvaem21 <salva.em21@gmail.com>
2024-06-07 09:38:51 +0200

    .JAR

--- tfg/src/test/java/com/salvaceloisma/tfg/TfgApplicationTests.java
+++ tfg/src/test/java/com/salvaceloisma/tfg/TfgApplicationTests.java
@@ -3,7 +3,7 @@ package com.salvaceloisma.tfg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PlantillaTFGApplication.class)
 class TfgApplicationTests {
 
 	@Test
--- tfg/src/test/java/org/sespejo/plantillasalva/PlantillasalvaApplicationTests.java
+++ tfg/src/test/java/org/sespejo/plantillasalva/PlantillasalvaApplicationTests.java
@@ -3,7 +3,9 @@ package org.sespejo.plantillasalva;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.salvaceloisma.tfg.PlantillaTFGApplication;
+
+@SpringBootTest(classes = PlantillaTFGApplication.class)
 class PlantillasalvaApplicationTests {
 
 	@Test
